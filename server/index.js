545c168e9176ca1039716dd32552a4a98a02503f
wizardcloth <ujjwal.chauhan8130@gmail.com>
2024-10-12 10:07:13 +0530

    completed pro-1

--- server/index.js
+++ server/index.js
@@ -2,15 +2,24 @@ import express, { json, urlencoded } from "express";
 import dotenv from "dotenv";
 import { database } from "./config/db.js";
 import router from "./router/products.route.js";
+import path from "path";
 const app = express();
 
 app.use(express.json());
 // app.use(urlencoded({extended:true}));
 
 dotenv.config();
-// console.log(process.env.url);
 
 app.use("/api/products",router);
+const __dirname = path.resolve();
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/client/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","dist","index.html"));
+    })
+}
+// console.log(process.env.url);
+
 
 let port = process.env.PORT || 8080;
 
